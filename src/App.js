43d5f2eb5edd9262952bedaee8ffebfaf76139d8
rajesh2724 <rajeshsri1436@gmail.com>
2024-06-22 18:49:41 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import vdo from './assets/bg vdo.mp4';
 import vdo1 from './assets/bg vdo 1.mp4'
 import NavBar from './pages/NavBar';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
-
-function App() {
-  return (
+export default function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='signup' element={<Signup/>}/>
+        <Route path='signin' element={<Signin/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
     <div className="App">
       <NavBar/>
       <div className='bgvdo'>
@@ -72,7 +83,3 @@ function App() {
           </div>
       </div>
     </div>
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,30 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter,RouterProvider}from "react-router-dom";
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>
-  },
-  {
-    path: "signin",
-    element:<Signin/>
-  },
-  {
-    path: "signup",
-    element:<Signup/>
-  }
-])
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
-);
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
